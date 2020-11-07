4d7368b1306f24ea0a5496bc7aa6871d8b4b16ad
RB-LE09 <74108345+RB-LE09@users.noreply.github.com>
2020-11-07 19:14:28 +0100

    Update co2zero.py
    - IP Adresse wird korrekt angezeigt
    - Zweite Displayzeile wird korrekt angesprochen
    - Temperatur wird ausgegeben
    - Uhrzeit wird ausgegeben
    
    - Optional könnte zweite Zeile auch für Temp. und Feuchtigkeit genutzt werden -> Uhrzeit könnte raus.

--- co2zero.py
+++ co2zero.py
@@ -1,9 +1,10 @@
 import time, signal, sys, socket
 from collections import deque
+from datetime import datetime
 
 # Temoperatursensor DHT11
 # https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/python-setup
-import board, adafruit_dht
+import board, Adafruit_DHT
 
 # Ampel
 # TODO https://gpiozero.readthedocs.io/en/stable/
@@ -21,7 +22,7 @@ def crude_progress_bar():
     sys.stdout.flush()
 
 # CO2-Sensor initialisieren
-sgp30.start_measurement(crude_progress_bar)
+# sgp30.start_measurement(crude_progress_bar)
 
 # Temperatursensor initialisieren
 dhtDevice = adafruit_dht.DHT11(board.D4)
@@ -73,19 +74,31 @@ def main():
             ppm = getPollutionData()
             ppmQueue.append(ppm)
             mov_average_ppm = int(sum(ppmQueue) / SLIDING_WINDOWS_SIZE)
+##### alter Teil von Hannes
+#            temperature_c = dhtDevice.temperature
+#            humidity = dhtDevice.humidity
 
-            temperature_c = dhtDevice.temperature
-            humidity = dhtDevice.humidity
+#            msg1 = "ppm: " + str(mov_average_ppm)
+#            msg2 = "{:.1f}C    {}% ".format(temperature_c, humidity)
+#            print(msg1 + ", " + msg2)
+#            lcd.lcd_string(msg1, lcd.LCD_LINE_1)
+#            lcd.lcd_string(msg2, lcd.LCD_LINE_2)
+##### Ende alter Teil 
 
-            msg1 = "ppm: " + str(mov_average_ppm)
-            msg2 = "{:.1f}C    {}% ".format(temperature_c, humidity)
-            print(msg1 + ", " + msg2)
-            lcd.lcd_string(msg1, lcd.LCD_LINE_1)
-            lcd.lcd_string(msg2, lcd.LCD_LINE_2)
-
-            if mov_average_ppm in range(0, 800):
+## neu von Michi
+            msg1 = "Aktuell: " + str(ppm)
+            msg2 = "CO2 Wert: " + str(mov_average_ppm)
+            temperatur = str(Adafruit_DHT.read_retry(11, 4))
+            now = datetime.now()
+            uhrzeit = now.strftime("%H:%M") 
+            msg3 = temperatur[7:-1] +"C  " + uhrzeit + " Uhr"
+            print(msg1 + ", " + msg2 + ", " + msg3)
+            lcd.lcd_string(msg2, 1)
+            lcd.lcd_string(msg3, 0xC0)
+ ### Ende neuer Teil       
+            if mov_average_ppm in range(0, 1000):
                 green()
-            elif mov_average_ppm in range(801, 2000):
+            elif mov_average_ppm in range(1001, 2000):
                 yellow()
             else:
                 red()
@@ -98,15 +111,16 @@ def main():
 if __name__ == "__main__":
     lcd.lcd_init()
 
-    print("CO2Zero starting, please wait...")
+    print("CO2Zero startet, bitte warten...")
 
-    # hostname = socket.gethostname()
-    # ip_address = socket.gethostbyname(hostname)
-    lcd.lcd_string("CO2Zero starting, please wait...", 1)
-    # lcd.lcd_string("IP: " + ip_address, 2)
-    #
-    # print("IP: " + ip_address)
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname + ".local")
+    lcd.lcd_string("CO2Zero startet, bitte warten...", 1)
+    lcd.lcd_string(ip_address, 0xC0)
+
+    print("IP: " + ip_address)
 
     time.sleep(2)
 
     main()
+
